929901e025dc5c628def26eedfaaf36d57a0564b
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-10-13 15:19:40 -0400

    colors

---
+++ public/colors.jsx
@@ -0,0 +1,10 @@
+const colors = {
+    LAVENDER_WEB:  '#E3DFFF',
+    LANGUID_LAVENDER: '#D3C0CD',
+    TUSCANY: '#B19994',
+    BEAVER: '#937666',
+    DARK_LIVER: '#3D3A4B'
+
+
+};
+export {colors};
