200137f5b729fb25fde9f0c65753ebd438efa2cc
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-11-18 13:39:30 -0500

    song card

--- src/profile/index.js
+++ src/profile/index.js
@@ -2,7 +2,11 @@ import Song from '../song/song.js'
 export const Profile = () => {
 
     return (
+        <div>
+            <h3> Sarah's Top Songs </h3>
         <Song/>
+
+        </div>
     )
 
 }
--- src/song/song.js
+++ src/song/song.js
@@ -1,17 +1,20 @@
+import Weeknd from '../images/weeknd.png'
+
 export const Song = () => {
 
     return (
         <ul class="list-group">
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                Cras justo odio
+                <img class="wd-song-image"src={Weeknd} alt="weeknd"/>
+                Scared to Live
                 <span class="badge bg-primary rounded-pill">14</span>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                Dapibus ac facilisis in
+                Bad Fruit
                 <span class="badge bg-primary rounded-pill">2</span>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                Morbi leo risus
+                Forever
                 <span class="badge bg-primary rounded-pill">1</span>
             </li>
         </ul>
