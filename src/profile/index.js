b12a0ec389ec1a6a07227fcacc1762c3c1caf31a
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-11-21 20:12:02 -0500

    profile stylings

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,11 +1,13 @@
-import Song from '../song/Song.js'
+import Song from '../song/song.js'
+import Sarah from '../images/sarah.jpg'
 export const Profile = () => {
 
     return (
         <div>
+            <img class="wd-prof-pic" src={Sarah}/>
             <h3> Sarah's Top Songs </h3>
-        <Song/>
 
+        <Song/>
         </div>
     )
 
--- src/song/song.js
+++ src/song/song.js
@@ -1,6 +1,9 @@
 import Weeknd from '../images/weeknd.png'
 import BadFruit from '../images/badfruit.jpg'
 import Forever from '../images/forever.jpg'
+import "../song/index.css";
+
+
 
 
 
@@ -8,21 +11,18 @@ export const Song = () => {
 
     return (
         <ul class="list-group">
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                <img class=" height:20px"src={Weeknd} alt="weeknd"/>
+            <li class="list-group-item">
+                <img class="wd-song-image"src={Weeknd} alt="weeknd"/>
                 Scared to Live
-                <span class="badge bg-primary rounded-pill">add to playlist</span>
             </li>
-            <li  class="list-group-item d-flex justify-content-between align-items-center">
-            <img  class="mw-100"src={BadFruit} alt="weeknd"/>
+            <li  class="list-group-item">
+            <img  class="wd-song-image" src={BadFruit} alt="weeknd"/>
 
                 Bad Fruit
-                <span class="badge bg-primary rounded-pill">add to playlist</span>
             </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-            <img class="mw-100"src={Forever} alt="weeknd"/>
+            <li class="list-group-item">
+            <img class="wd-song-image" src={Forever} alt="weeknd"/>
                 Forever
-                <span class="badge bg-primary rounded-pill">add to playlist</span>
             </li>
         </ul>
     )
